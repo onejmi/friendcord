29116a69aa33582336cc7760597ff4a891865443
scarger <supremeterminal@gmail.com>
2021-04-13 15:06:10 -0400

    setup mongodb client

--- api/server.ts
+++ api/server.ts
@@ -2,16 +2,27 @@ import express from 'express'
 import * as controllers from './controllers';
 import { Server } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
+import { MongoClient } from 'mongodb'
+import secrets from '../secret'
 
 class ApiServer extends Server {
 
     private readonly SERVER_STARTED = 'API server started on port: ';
+
+    private static readonly uri = `mongodb+srv://${secrets.dbUser}:${secrets.dbPassword}@cluster0.breea.mongodb.net/${secrets.dbName}?retryWrites=true&w=majority`
+    private static _client = new MongoClient(ApiServer.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    public static get db() {
+        return this._client.db();
+    }
     
     constructor() {
         super(true);
         this.app.use(express.json());
         this.app.use(express.urlencoded({extended: true}));
-        this.setupControllers();
+        ApiServer._client.connect().then((_) => {
+            this.setupControllers();
+        })
     }
 
     private setupControllers(): void {
